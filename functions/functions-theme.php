e518a662df6e92932c719f93f6cf890c202b0b8d
georgesboris <georgesboris@gmail.com>
2016-03-30 07:35:58 -0300

    added body-color-blend lib for scroll effect

--- functions/functions-theme.php
+++ functions/functions-theme.php
@@ -10,6 +10,10 @@
         wp_enqueue_script( 'animate-background-image' );
         wp_register_script( 'animate-classes', get_template_directory_uri().'/assets/scripts/lib/animate-classes.js', array(), '1.0', true );
         wp_enqueue_script( 'animate-classes' );
+        wp_register_script( 'scroll-trigger', get_template_directory_uri().'/assets/scripts/lib/scroll-trigger.js', array(), '1.0', true );
+        wp_enqueue_script( 'scroll-trigger' );
+        wp_register_script( 'body-color-blend', get_template_directory_uri().'/assets/scripts/lib/body-color-blend.js', array(), '1.0', true );
+        wp_enqueue_script( 'body-color-blend' );
 
         wp_register_script( 'menu', get_template_directory_uri().'/assets/scripts/menu.js', array( 'animate-classes' ), '1.0', true );
         wp_enqueue_script( 'menu' );
--- templates/index.php
+++ templates/index.php
@@ -1,8 +1,27 @@
 <?php oed_helpers:: get_template_parts( array('_parts/html-header', '_parts/header') ); ?>
 
-<section class='landing-section'>
-  <header>
-  </header>
+<section
+  class='hero'
+  data-color-point='240, 240, 240'>
+  section
+</section>
+
+<section
+  class='hero'
+  data-color-point='0, 0, 0'>
+  section
+</section>
+
+<section
+  class='hero'
+  data-color-point='177, 214, 152'>
+  section
+</section>
+
+<section
+  class='hero'
+  data-color-point='177, 194, 195'>
+  section
 </section>
 
 <?php oed_helpers:: get_template_parts( array('_parts/footer', '_parts/html-footer') ); ?>
